32caee09b053a0db568648ab9bd4673492b6e1fa
samuu1i <elitete23153@gmail.com>
2025-09-18 21:09:24 -0300

    mejorando ando

--- codigo/juego/fantasma.c
+++ codigo/juego/fantasma.c
@@ -15,9 +15,11 @@ void dibujarFantasma(tFantasma* fantasma, size_t fila, size_t columna)
 }
 
 // Si se necesita, agregar más parametros
-int calcularMovimientoFantasma(tFantasma* fantasma, const tLaberinto* laberinto, const tJugador* jugador)
+char calcularMovimientoFantasma(tFantasma* fantasma, const tLaberinto* laberinto, const tJugador* jugador)
 {
     //matriz que dice en que posicion se moveria
+    int i, pos = -1;
+    int distMin = MAX_DIST;
     int movimiento[4][2] =
     {
         {-1,0},
@@ -26,24 +28,59 @@ int calcularMovimientoFantasma(tFantasma* fantasma, const tLaberinto* laberinto,
         {0,1}
     };
     //con esto voy a ver si tiene pared o no
-    int validas[] = {0,0,0,0};
+    int nuevaPos[4][2] =
+    {
+        {-1,-1},
+        {-1,-1},
+        {-1,-1},
+        {-1,-1}
+    };
 
     int nuevaFila = fantasma->filaActual;
     int nuevaColumna = fantasma->columnaActual;
-
+    int dist;
     //reviso cada posicion de la matriz para ver a donde se mueve y veo si tiene pared o se pasa del limite del mapa
     //si no tiene nada malo me fijo en la posicion en la que estuvo y despues calculo si era derecha, izq etc
-    for(int i = 0; i < 4; i++)
+    for(i = 0; i < 4; i++)
     {
         nuevaFila = fantasma->filaActual + movimiento[i][0];
         nuevaColumna = fantasma->columnaActual + movimiento[i][1];
 
-        if(nuevaFila >= 0 && nuevaFila <= laberinto->filas && nuevaColumna >= 0 &&
-           nuevaColumna <= laberinto->columnas && laberinto->casillas[nuevaFila][nuevaColumna] != '#')
+        if(nuevaFila > 0 && nuevaFila < laberinto->filas && nuevaColumna > 0 &&
+                nuevaColumna < laberinto->columnas && laberinto->casillas[nuevaFila][nuevaColumna] != '#'
+                && laberinto->casillas[nuevaFila][nuevaColumna] != 'F')
         {
-            validas[i] = 1;
+            //validas[i] = 1;
+            nuevaPos[i][0] = nuevaFila;
+            nuevaPos[i][1] = nuevaColumna;
         }
     }
+
+    for(i = 0; i < 4; i++)
+    {
+        if(nuevaPos[i][0] != -1)
+        {
+            dist = abs(nuevaPos[i][0] - jugador->filaActual) + abs(nuevaPos[i][1] - jugador->columnaActual);
+
+            if(dist < distMin)
+            {
+                distMin = dist;
+                pos = i;
+            }
+
+        }
+    }
+    if (pos == -1) {
+
+             return FALSO; }
+
+
+    if (nuevaPos[pos][0] > fantasma->filaActual) return ABAJO;
+    if (nuevaPos[pos][0] < fantasma->filaActual) return ARRIBA;
+    if (nuevaPos[pos][1] > fantasma->columnaActual) return DERECHA;
+    if (nuevaPos[pos][1] < fantasma->columnaActual) return IZQUIERDA;
+
+    return FALSO;
     // No permitir moverse si hay una pared
 
 
--- codigo/juego/fantasma.h
+++ codigo/juego/fantasma.h
@@ -1,5 +1,6 @@
 #ifndef FANTASMA_H_INCLUDED
 #define FANTASMA_H_INCLUDED
+#define MAX_DIST 100
 
 #include "laberinto.h"
 #include "jugador.h"
@@ -15,7 +16,7 @@ typedef struct
 
 void crearFantasma(tFantasma* fantasma, size_t filaInicial, size_t columnaInicial);
 void dibujarFantasma(tFantasma* fantasma, size_t fila, size_t columna);
-int calcularMovimientoFantasma(tFantasma* fantasma, const tLaberinto* laberinto, const tJugador* jugador);
+char calcularMovimientoFantasma(tFantasma* fantasma, const tLaberinto* laberinto, const tJugador* jugador);
 int moverFantasma(tFantasma* fantasma, char direccion, const tLaberinto* laberinto);
 
 #endif // FANTASMA_H_INCLUDED
