28102f40f8eb305b372c05a4e795f0241c9a6496
VlaF04 <vladimirfranciscocarp@gmail.com>
2025-09-20 14:52:29 -0300

    hola

---
+++ codigo/juego/premios_y_vidas.h
@@ -0,0 +1,31 @@
+#ifndef PREMIOS_Y_VIDAS_H_INCLUDED
+#define PREMIOS_Y_VIDAS_H_INCLUDED
+
+#include <stdio.h>
+#include <time.h>
+#include "laberinto.h"
+#include "jugador.h"
+
+#define PUNTAJE_LOW 150
+#define PUNTAJE_MID 300
+#define PUNTAJE_HIGH 700
+
+#define OPCION_LIMITE 7
+#define MIN_LOW 1
+#define MAX_LOW 3
+#define MIN_MID 4
+#define MAX_MID 5
+
+#define TOPE_VIDAS 99
+
+void vidasYPuntos(tJugador * jugador);
+
+unsigned short chequeoPremio (tJugador * jugador, tLaberinto * laberinto);
+void sumarPuntaje (tJugador * jugador, tLaberinto * laberinto);
+
+void ponerCamino (tJugador * jugador, tLaberinto * laberinto);
+
+unsigned short chequeoVida (tJugador * jugador, tLaberinto * laberinto);
+void sumarVida (tJugador * jugador);
+
+#endif // PREMIOS_Y_VIDAS_H_INCLUDED
